8303d564e7ed3147c9e00ee5b65f9b9c1f7d5733
pm91680 <55507989+pm91680@users.noreply.github.com>
2019-09-18 23:44:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Prajjwal Maheshwari</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<body style="background-color:orange;">
+<h1>Welcome to  Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a>
+</div>
+</body>
